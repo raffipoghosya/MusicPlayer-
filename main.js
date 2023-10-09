d36308404608e619047c2489c668361702028a61
Imac <imac@Imacs-MacBook-Air.local>
2023-10-09 23:52:06 +0400

    Get a number between 64 to 256 (for getting lighter colors)

--- main.js
+++ main.js
@@ -43,7 +43,10 @@ let track_list = [
 
 function random_bg_color() {
 
-
+  // Get a number between 64 to 256 (for getting lighter colors)
+  let red = Math.floor(Math.random() * 256) + 64;
+  let green = Math.floor(Math.random() * 256) + 64;
+  let blue = Math.floor(Math.random() * 256) + 64;
 
   // Construct a color withe the given values
   let bgColor = "rgb(" + red + "," + green + "," + blue + ")";
